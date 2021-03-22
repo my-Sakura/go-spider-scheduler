08334decc088942d664932aff37b2eaa076e954b
my-Sakura <69021934+my-Sakura@users.noreply.github.com>
2021-03-22 16:38:13 +0800

    add shibor API

--- api/shibor.go
+++ api/shibor.go
@@ -4,7 +4,7 @@ import (
 	"encoding/json"
 	"net/http"
 
-	"github.com/my-Sakura/go-spider-scheduler/shibor/api/service/structure/shibor"
+	"github.com/my-Sakura/go-spider-scheduler/api/service/structure/shibor"
 )
 
 func Shibor(w http.ResponseWriter, r *http.Request) {
